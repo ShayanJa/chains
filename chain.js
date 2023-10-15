83b91ec7a5a3d4eceba980beeb79e5d0314df965
shayanja <shay.talebi@gmail.com>
2023-10-15 16:27:44 -0500

    Fix chain.js with regex to add networks

--- chain.js
+++ chain.js
@@ -8,89 +8,101 @@ const listChains = async () => {
       return response.data.map((chain) => chain.name);
     }
   } catch {
-    return [];
+    chains = require("./chains.json");
+    return chains.map((chain) => chain.name);
   }
 };
 
 const addChain = async (chainName) => {
+  let chainData;
   try {
     const response = await axios.get(`https://chainid.network/chains.json`);
-    data = response.data || require("./chains.json");
-    if (data) {
-      const chainInfo = data.find(
-        (chain) =>
-          chain.name.toLowerCase() === chainName.toLowerCase() ||
-          chain.chain.toLowerCase() === chainName.toLowerCase()
-        // || chain.icon.toLowerCase() === chainName.toLowerCase()
-      );
-      console.log(chainInfo);
-      if (chainInfo) {
-        console.log("chaininfo");
-        const configPath = "./hardhat.config.js";
-        if (fs.existsSync(configPath)) {
-          console.log("here");
+    chainData = response.data;
+  } catch {
+    chainData = require("./chains.json");
+    console.error(`Error fetching chain information: Using Local...`);
+  }
+  if (chainData) {
+    console.log("Found data");
+    const chainInfo = chainData.find(
+      (chain) =>
+        chain.name.toLowerCase() === chainName.toLowerCase() ||
+        chain.chain.toLowerCase() === chainName.toLowerCase() ||
+        chain.shortName.toLowerCase() === chainName.toLowerCase()
+    );
+    console.log(chainInfo);
+    if (chainInfo) {
+      const configPath = "./hardhat.config.js" || "";
+      if (fs.existsSync(configPath)) {
+        fs.readFile(configPath, "utf8", (err, data) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          // Parse the existing module.exports statement
+          let existingObject;
+          let updatedObject;
+          try {
+            // Assuming module.exports is on the last line of the file
+            console.log(data);
+            //FIx regex to get proper export statement
+            const exportsStatement = data.match(
+              /module\.exports\s*=\s*({[\s\S]*?});/
+            );
+            console.log("export Statement", exportsStatement);
+            existingObject = eval("(" + exportsStatement[1] + ")");
+            console.log("exisiting Module", existingObject);
+          } catch (parseError) {
+            console.log(data);
+            console.error(
+              "Error parsing module.exports statement:",
+              parseError
+            );
+            return;
+          }
+          if (existingObject?.networks) {
+            existingObject.networks[chainName] = {
+              chainId: chainInfo.chainId,
+              url: chainInfo.rpc[0],
+            };
 
-          fs.readFile(configPath, "utf8", (err, data) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-            // Parse the existing module.exports statement
-            let existingObject;
-            try {
-              // Assuming module.exports is on the last line of the file
-              const exportsStatement = data.match(
-                /module\.exports\s*=\s*(\{[^\}]+\});/
-              )[1];
-              existingObject = eval("(" + exportsStatement + ")");
-              console.log(existingObject);
-            } catch (parseError) {
-              console.error(
-                "Error parsing module.exports statement:",
-                parseError
-              );
-              return;
-            }
+            updatedObject = existingObject;
+          } else {
             // Merge the new object with the existing object
-            const updatedObject = {
+            console.log("here2");
+            updatedObject = {
               ...existingObject,
               ...{
-                config: {
-                  networks: {
-                    chainName: {
-                      chainId: chainInfo.chainId,
-                      url: chainInfo.rpc[0],
-                    },
+                networks: {
+                  [chainName]: {
+                    chainId: chainInfo.chainId,
+                    url: chainInfo.rpc[0],
                   },
                 },
               },
             };
-            // Replace the existing module.exports statement with the updated object
-            const updatedContent = data.replace(
-              /module\.exports\s*=\s*\{[^\}]+\};/,
-              "module.exports = " + JSON.stringify(updatedObject, null, 2) + ";"
-            );
+          }
+          // Replace the existing module.exports statement with the updated object
+          const updatedContent = data.replace(
+            /module\.exports\s*=\s*({[\s\S]*?});/,
+            "module.exports = " + JSON.stringify(updatedObject, null, 2) + ";"
+          );
 
-            // Write the updated content back to the file
-            fs.writeFile(configPath, updatedContent, "utf8", (err) => {
-              if (err) {
-                console.error("Error writing file:", err);
-              } else {
-                console.log("File updated successfully!");
-              }
-            });
+          // Write the updated content back to the file
+          fs.writeFile(configPath, updatedContent, "utf8", (err) => {
+            if (err) {
+              console.error("Error writing file:", err);
+            } else {
+              console.log("File updated successfully!");
+            }
           });
-          // else {
-          //   console.log("hardhat.config.js not found in the current directory");
-          // }
-        }
-        // else {
-        //   console.log(`No chain found with the name '${chainName}'`);
-        // }
+        });
+      } else {
+        console.log("hardhat.config.js not found in the current directory");
       }
+    } else {
+      console.log(`No chain found with the name '${chainName}'`);
     }
-  } catch (error) {
-    console.error(`Error fetching chain information: ${error}`);
   }
 };
 
@@ -102,8 +114,13 @@ const main = async () => {
   } else if (userArgs[0] == "list") {
     chains = await listChains();
     console.log(chains);
+  } else if (userArgs[0] == "help") {
+    console.log("Available Commands:");
+    console.log("list");
+    console.log("add");
   } else {
     console.log("Usage: chains.js add <chain_name>");
+    console.log("Usage: chains.js list:");
   }
 };
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,5 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  "solidity": "0.8.19",
-  "config": {
-    "networks": {
-      "chainName": {
-        "chainId": 4,
-        "url": "https://rinkeby.infura.io/v3/${INFURA_API_KEY}"
-      }
-    }
-  }
+  solidity: "0.8.19",
 };
